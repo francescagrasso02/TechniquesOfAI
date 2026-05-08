fix : weather is initialized at the begining, corrected cost formula. feat : plot method to preview city grid

grid.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import copy
+from pyvis.network import Network
+
 
 #Only for test 
+np.random.seed(42)
 weights = {"w":0.5,"l":0.1,"s":0.3,"sf":0.2}
+weather = np.random.randint(0,5) #Weather condition ["Sunny","Moderate Rain","Moderate Snow","Extreme Rain","Extreme Snow"]
 
 class CityGrid():
     def __init__(self,size):
@@ -47,46 +52,79 @@ class CityGrid():
     def generate_attributes(self,seed):
         """
         Generates random attributes for each 
-        node on the grid. By default a uniform 
+        edge on the grid. By default a uniform 
         distribution was chosen, this should be reconsidered 
         when we'll do experiments
         
         :param self: CityGrid object
         :param seed: seed value 
         """
-        np.random.seed(seed)
-        attributes = np.zeros((self.size**2,9))
+        keys = list(self.graph.keys())
+        dict_attributes = {}
+        try : 
+            for node in keys:
+                for neighbor in self.graph[node]:
+                    edge_attributes = []            
+                    edge_attributes.append(np.random.uniform(4,18.5)) #width in m
+                    edge_attributes.append(np.random.uniform(200,2000)) #lenght in m
+                    edge_attributes.append(np.random.uniform(0.7,3.9)) #Horizontal slope in °
+                    edge_attributes.append(np.random.uniform(0,2.5)) #Cross slope in °
+                    edge_attributes.append(np.random.choice([0,1])) #Kerb (accessible or not)
+                    edge_attributes.append(np.random.randint(0,5)) #Surface type ["Concrete","Asphalt","Brick","Cobblestone","Gravel"]
+                    edge_attributes.append(weather) #Weather condition ["Sunny","Moderate Rain","Moderate Snow","Extreme Rain","Extreme Snow"]
+                    edge_attributes.append(np.random.choice([0,1])) #Traversability depends on crowd value that should change during the experiment for MPD, constant for search
+                    edge_attributes.append(np.random.choice([0,1])) #Tactile pavement
+                    dict_attributes[(node,neighbor)] = edge_attributes
+        except Exception as e : 
+            print(f"Error, {e}")
 
-        attributes[:,0]=np.random.uniform(4,18.5,size=self.size**2) #width in m
-        attributes[:,1]=np.random.uniform(200,2000,size=self.size**2) #lenght in m
-        attributes[:,2]=np.random.uniform(0.7,3.9,size=self.size**2) #Horizontal slope in °
-        attributes[:,3]=np.random.uniform(0,2.5,size=self.size**2) #Cross slope in °
-        attributes[:,4]=np.random.choice([0,1],size=self.size**2) #Kerb (accessible or not)
-        attributes[:,5]=np.random.randint(0,5,size=self.size**2) #Surface type ["Concrete","Asphalt","Brick","Cobblestone","Gravel"]
-        attributes[:,6]=np.random.randint(0,5,size=self.size**2) #Weather condition ["Sunny","Moderate Rain","Moderate Snow","Extreme Rain","Extreme Snow"]
-        attributes[:,7]=np.random.choice([0,1],size=self.size**2) #Traversability depends on crowd value that should change during the experiment for MPD, constant for search
-        attributes[:,8]=np.random.choice([0,1],size=self.size**2) #Tactile pavement
+        self.attributes = dict_attributes
 
-        self.attributes = attributes
-
-        return attributes
+        return dict_attributes
     
-    def get_cost(self,w):
+    def get_cost(self,w,weather):
         """
         Computes cost of each node depending on the chosen weights
         
         :param self: CityGrid object
         :param w: weights dictionary : w = {"w":w0,"l":w1,"s":w2,"sf":w3}
+        :param weather: weather state (initialized at the begining)
         """
-        s = (self.attributes - self.attributes.min(axis=0))/(self.attributes.max(axis=0) - self.attributes.min(axis=0)+1e-9)
+        dict_cost = {}  
+        keys_mapping = list(self.attributes.keys())
+        
+        arr_attributes = np.array([self.attributes[key] for key in keys_mapping])
+        
+        s = (arr_attributes - arr_attributes.min(axis=0))/(arr_attributes.max(axis=0) - arr_attributes.min(axis=0)+1e-9)
         self.score = s
-        cost = (w["w"]*s[:,[0]] + w["l"]*s[:,[1]] + w["s"]*(s[:,[2]]+s[:,[3]])*s[:,[6]] + w["sf"]*s[:,[6]])
-        self.cost = cost
-
-        return cost
+        cost = (w["w"] * s[:,0] + w["l"] * s[:,1] + w["s"] * s[:,2] * s[:,6] + w["sf"] * s[:,3] * s[:,6])
+        for k in range(len(keys_mapping)):
+            dict_cost[keys_mapping[k]] = cost[k]
+        self.cost = dict_cost
     
-city = CityGrid(size=8)
+    def plot_city_grid(self, filename="map.html"):
+        """
+        Generates an html file displaying the graph 
+        :param self: CityGrid object
+        :filename: file name (.html)
+        """
+        net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white", directed=True, notebook=False)
+        
+        for i in range(self.size**2):
+            row, col = i // self.size, i % self.size
+            net.add_node(i, label=str(i), x=col*200, y=row*200, physics=False)
+
+        for (u, v), val in self.cost.items():
+            c_norm = max(0, min(1, val)) 
+            color = f"rgba({int(255*c_norm)}, {int(255*(1-c_norm))}, 0, 1)"
+            title = f"Cost: {val:.2f}"
+            net.add_edge(u, v, color=color, width=2 + (val * 5), title=title)
+
+        net.write_html(filename)
+
+
+city = CityGrid(size=10)
 city.generate_attributes(42)
-city.get_cost(weights)
-city.plot_city_grid()
+city.get_cost(weights,weather)
+city.plot_city_grid(filename="map.html")
 